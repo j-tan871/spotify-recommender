40189ca04515520bab420361c08d580716c5ac75
j-tan871 <60245545+j-tan871@users.noreply.github.com>
2021-05-03 23:24:40 -0500

    search endpoint

--- server/app.py
+++ server/app.py
@@ -1,7 +1,28 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
+from util import Util
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+util = Util()
+sp = util.setup()
 
 @app.route('/')
+@cross_origin()
 def index():
     return 'Hello World!'
+
+@app.route('/search/<string:name>')
+@cross_origin()
+def search(name):
+    return util.find_artists(sp, name)
+
+@app.route('/results/<string:artist>')
+@cross_origin()
+def results(artist):
+    pass
+
+if __name__ == "__main__":
+    app.run()
