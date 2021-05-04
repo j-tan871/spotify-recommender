e138dcbd210da72d07c620b2dab49d695a2cfa56
j-tan871 <60245545+j-tan871@users.noreply.github.com>
2021-05-04 11:10:16 -0500

    home page

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,30 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'class', // or 'media' or 'class'
+  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      }, 
+    },
+    colors: {
+      darkblue: {
+        DEFAULT: '#0E2550'
+      }, 
+      pink: {
+        DEFAULT: '#FFA7A7'
+      }, 
+      mediumpink: {
+        DEFAULT: '#FFD8D8'
+      }, 
+      lightpink: {
+        DEFAULT: '#FFEAEA'
+      }, 
+      white: colors.white,
+    }
   },
   variants: {
     extend: {},
