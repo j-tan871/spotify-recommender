ed061fe5dd3d299c14e8a3c7a2d222040f7aee0e
j-tan871 <60245545+j-tan871@users.noreply.github.com>
2021-05-17 14:50:26 -0500

    remove info description

--- client/pages/info.js
+++ client/pages/info.js
@@ -11,16 +11,20 @@ export default function Info() {
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700;800&display=swap" rel="stylesheet" />
       </Head>
       <main className='p-12 lg:pt-24 lg:mx-24 lg:w-1/2'>
-        <h2 className='mb-2 md:mb-5'>Calculating song similarity</h2>
-        <p className='text-white pb-2 md:text-lg'>
+        <h2>Calculating song similarity</h2>
+        <br />
+        <p className='text-white md:text-lg'>in progress</p>
+        {/* <p className='text-white md:text-lg'>
           The K-means algorithm and K-nearest neighbors algorithm are used to calculate song similarity and return results. 
         </p>
-        <p className='text-white pb-2 md:text-lg'>
+        <br />
+        <p className='text-white md:text-lg'>
           The Spotify API returns characteristics of songs, such as danceability, energy, speechiness, acousticness, etc. 
         </p>
-        <p className='text-white pb-2 md:text-lg'>
+        <br />
+        <p className='text-white md:text-lg'>
           Songs that are on average nearer to your current top 10 songs, or the characteristics that you input, are ranked higher in your results. 
-        </p>
+        </p> */}
         <button onClick={() => router.back()}>I want to discover more songs!</button>
       </main>
     </div>
